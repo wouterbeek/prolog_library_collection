23a12a68ce6c761cabf5ad00b53624580d469858
Wouter Beek <wouter@wouterbeek.com>
2015-10-16 13:48:19 +0200

    CLEAN: Rewrote emoticon parser.

--- tmp/dcg/emoticons/emoticons.pl
+++ tmp/dcg/emoticons/emoticons.pl
@@ -1,7 +1,7 @@
 :- module(
   emoticons,
   [
-    emote//1
+    emote//1 % -Meaning:string
   ]
 ).
 
@@ -11,98 +11,101 @@ These are mostly useful for emoticons seen in prolog code.
 Japanese (^_^) style emoticons not covered.
 
 @author Anne Ogborn
+@author Wouter Beek
 @tbd various IM client icons.
 @tbd Consider moving all this to a special annie emoticon dcg
 @tbd Japanese (^_^) style emoticons
-@version 2013
+@version 2013, 2015/10
 */
 
-:- use_module(library(lists), except([delete/3,subset/2])).
+:- use_module(library(dcg/dcg_content)).
+:- use_module(library(lists)).
+:- use_module(library(string_ext)).
 
 
+
+
+
+%! emote(-Meaning:string)// .
 
 emote("Annie is her usual happy self") --> "8cD".
-emote(General) -->
+emote(M) -->
   "-C:^{",
   part(mouth, Say),
-  {format(codes(General), 'The General, ~s', [Say])}.
-
-emote(A) -->
-  part(hat, H),
-  part(eyebrows, Eb),
-  part(eyes, E),
-  part(nose, N),
-  part(mouth, M),
-  {format(codes(A), '~s ~s ~s ~s ~s', [H,Eb,E,N,M])}.
-
+  {string_concat("The General, ", Say, M)}.
+emote(M) -->
+  hat(M1),
+  eyebrows(M2),
+  eyes(M3),
+  nose(M4),
+  mouth(M5),
+  {string_list_concat([M1,M2,M3,M4,M5], " ", M)}.
 emote("darned if I know, half the time they confuse me too") --> ... .
 
-... --> [].
-... --> [_], ... .
 
-part(Type, Say) -->
-  {meaning(Type, X, Say)},
-  X.
 
-meaning(hat, "", "").
-meaning(hat, "d", "baseball cap worn sideways, or hat in general").
-meaning(hat, "&", "brain (brainy, or fried), or hair").
-meaning(hat, ")&$#|", "Carmen Miranda Hat").
-meaning(hat, "<", "Dunce cap, pointed head, or thinking cap").
-meaning(hat, ">", "Antenna").
-meaning(hat, "=", "hair standing on end").
-meaning(hat, "", "").
-meaning(
-  eyebrows,
-  ">",
-  "inner eyebrows down, expressing worry or consternation"
-).
-meaning(eyebrows, "|", "eyebrows corrugated, expressing sternness").
-meaning(eyebrows, "", "").
-meaning(eyes, "8", "").
-meaning(eyes, "B", "sunglasses").
-meaning(eyes, ":", "beady eyes").
-meaning(eyes, "X", "eyes closed or covered, in grief, disbelief, or pain").
-meaning(eyes, "o", "I'm a cyclops").
-meaning(eyes, "88", "wearing glasses").
-meaning(eyes, ";", "winking").
-meaning(nose, "c", "").
-meaning(nose, "C", "big nose").
-meaning(nose, "c:", "button nose").
-meaning(nose, [X], "non annie nose, reference to others"):-
-  member(X, `2^o-uUOvV>`).
-meaning(mouth, "*", "kissing").
-meaning(mouth, "+", "puckered mouth").
-meaning(mouth, "(", "sad").
-meaning(mouth, ")", "happy, smile").
-meaning(mouth, "E", "toothy (maybe gap tooth dumb)").
-meaning(mouth, "o", "o mouth, suprise or shock").
-meaning(mouth, "O", "mouth agape, shock").
-meaning(mouth, "[", "stern or painful grimace").
-meaning(mouth, "]", "trying not to laugh, or smile").
-meaning(mouth, "|", "closed mouth look").
-meaning(mouth, "S", "discomfort, pain").
-meaning(
-  mouth,
-  "d",
-  "tongue hanging out (delicious, distaste, or concentration)"
-).
-meaning(mouth, "D", "Very happy - or default").
-meaning(mouth, "F", "Vampire fangs").
-meaning(mouth, "7", "variant mouth").
-meaning(mouth, "X", "puckered mouth - pain, good grief, or ouch").
-meaning(mouth, "C", "sticking out lower lip").
-meaning(mouth, ">", "variant smile").
-meaning(mouth, "?", "puzzlement, empathy, or concern").
-meaning(
-  mouth,
-  "p",
-  "tongue hanging out (delicious, distaste, or concentration)"
-).
-meaning(
-  mouth,
-  "P",
-  "tongue hanging out (delicious, distaste, or concentration)"
-).
-meaning(mouth, [X], "irony"):-
-  member(X, `/\\`).
+%! eyebrows(-Meaning:string)// .
+
+eyebrows("inner eyebrows down, expressing worry or consternation") --> ">".
+eyebrows("eyebrows corrugated, expressing sternness") --> "|".
+
+
+
+%! eyes(-Meaning:string)// .
+
+eyes("") --> "8".
+eyes("sunglasses") --> "B".
+eyes("beady eyes") --> ":".
+eyes("eyes closed or covered, in grief, disbelief, or pain") --> "X".
+eyes("I'm a cyclops") --> "o".
+eyes("wearing glasses") --> "88".
+eyes("winking") --> ";".
+
+
+
+%! hat(-Meaning:string)// .
+
+hat("baseball cap worn sideways, or hat in general") --> "d".
+hat("brain (brainy, or fried), or hair") --> "&".
+hat("Carmen Miranda Hat") --> ")&$#|".
+hat("Dunce cap, pointed head, or thinking cap") --> "<".
+hat("Antenna") --> ">".
+hat("hair standing on end") --> "=".
+
+
+
+%! mouth(-Meaning:string)// .
+
+mouth("kissing") --> "*".
+mouth("puckered mouth") --> "+".
+mouth("sad") --> "(".
+mouth("happy, smile") --> ")".
+mouth("toothy (maybe gap tooth dumb)") --> "E".
+mouth("o mouth, suprise or shock") --> "o".
+mouth("mouth agape, shock") --> "O".
+mouth("stern or painful grimace") --> "[".
+mouth("trying not to laugh, or smile") --> "]".
+mouth("closed mouth look") --> "|".
+mouth("discomfort, pain") --> "S".
+mouth("tongue hanging out (delicious, distaste, or concentration)") --> "d".
+mouth("Very happy - or default") --> "D", 
+mouth("Vampire fangs") --> "F", 
+mouth("variant mouth") --> "7".
+mouth("puckered mouth - pain, good grief, or ouch") --> "X".
+mouth("sticking out lower lip") --> "C".
+mouth("variant smile") --> ">".
+mouth("puzzlement, empathy, or concern") --> "?".
+mouth("tongue hanging out (delicious, distaste, or concentration)") --> "p".
+mouth("tongue hanging out (delicious, distaste, or concentration)") --> "P".
+mouth("irony") --> "/".
+mouth("irony") --> "\\".
+
+
+
+%! nose(-Meaning:string)// .
+
+nose("") --> "c".
+nose("big nose") --> "C".
+nose("button nose") --> "c:".
+nose("non annie nose, reference to others") -->
+  ("2" ; "^" ; "o" ; "-" ; "u" ; "U" ; "O" ; "v" ; "V" ; ">").
