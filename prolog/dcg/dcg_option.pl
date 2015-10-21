4b97076c52d9df174c33a0f7c00dc08154de8cba
Wouter Beek <me@wouterbeek.com>
2015-10-21 14:16:15 +0200

    CLEAN: Renamed if_option//3 -> dcg_if_option//3. CLEAN: Renamed if_select_option//3 -> dcg_if_select_option//3.

--- prolog/dcg/dcg_option.pl
+++ prolog/dcg/dcg_option.pl
@@ -1,13 +1,13 @@
 :- module(
   dcg_option,
   [
-    if_option//3, % +Option:compound
-                  % +Options:list(compound)
-                  % :Dcg
-    if_select_option//4 % +Option:compound
-                        % +Options:list(compound)
-                        % -RestOptions:list(compound)
-                        % :Dcg
+    dcg_if_option//3, % +Option:compound
+                      % +Options:list(compound)
+                      % :Dcg_2
+    dcg_if_select_option//4 % +Option:compound
+                            % +Options:list(compound)
+                            % -RestOptions:list(compound)
+                            % :Dcg_2
   ]
 ).
 
@@ -18,35 +18,35 @@ Handling of options for DCG rules.
 This module is intended to 
 
 @author Wouter Beek
-@version 2015/08
+@version 2015/08, 2015/10
 */
 
 :- use_module(library(option)).
 
-:- meta_predicate(if_option(+,+,//,?,?)).
-:- meta_predicate(if_select_option(+,+,-,//,?,?)).
+:- meta_predicate(dcg_if_option(+,+,//,?,?)).
+:- meta_predicate(dcg_if_select_option(+,+,-,//,?,?)).
 
 
 
 
 
-%! if_option(+Option:compound, +Options:list(compound), :Dcg_0)// is det.
+%! dcg_if_option(+Option:compound, +Options:list(compound), :Dcg_2)// is det.
 
-if_option(Opt, Opts, Dcg_0) -->
-  {option(Opt, Opts)}, !,
-  Dcg_0.
-if_option(_, _, _) --> [].
+dcg_if_option(Option, Options, Dcg_2) -->
+  {option(Option, Options)}, !,
+  Dcg_2.
+dcg_if_option(_, _, _) --> [].
 
 
 
-%! if_select_option(
+%! dcg_if_select_option(
 %!   +Option:compound,
 %!   +Options:list(compound),
 %!   -RestOptions:list(compound),
-%!   :Dcg_0
+%!   :Dcg_2
 %! )// is det.
 
-if_select_option(Opt, Opts1, Opts2, Dcg_0) -->
+dcg_if_select_option(Opt, Opts1, Opts2, Dcg_2) -->
   {select_option(Opt, Opts1, Opts2)}, !,
-  Dcg_0.
-if_select_option(_, Opts, Opts, _) --> [].
+  Dcg_2.
+dcg_if_select_option(_, Opts, Opts, _) --> [].
