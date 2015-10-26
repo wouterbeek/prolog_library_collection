a785319e1e4665e68cc78d35e2cecff25f0a1ada
Wouter Beek <me@wouterbeek.com>
2015-10-26 11:13:56 +0100

    ADDED: open_pdf/2, allowing options to be passed to run_process/3.

--- prolog/os/pdf.pl
+++ prolog/os/pdf.pl
@@ -1,7 +1,9 @@
 :- module(
   pdf,
   [
-    open_pdf/1 % +File:atom
+    open_pdf/1, % +File:atom
+    open_pdf/2 % +File:atom
+               % +Options:list(compound)
   ]
 ).
 
@@ -10,11 +12,17 @@
 Support for opening PDF files.
 
 @author Wouter Beek
+@license MIT License
 @version 2015/10
 */
 
+:- use_module(library(option)).
 :- use_module(library(os/external_program)).
 :- use_module(library(os/process_ext)).
+
+:- predicate_options(open_pdf/2, 2, [
+     pass_to(run_process/3, 3)
+   ]).
 
 :- dynamic(user:file_type_program/2).
 :- multifile(user:file_type_program/2).
@@ -27,8 +35,17 @@ user:file_type_program(pdf, xpdf).
 
 
 %! open_pdf(+File:atom) is det.
-% Opens the given PDF file.
+% Wrapper around open_pdf/2 with default options.
 
 open_pdf(File):-
+  open_pdf(File, [detached(true)]).
+
+
+%! open_pdf(+File:atom, +Options:list(compound)) is det.
+% Opens the given PDF file.
+% Options are passed to run_process/3.
+
+open_pdf(File, Opts1):-
+  merge_options(Opts1, [program(Program)], Opts2),
   once(find_program_by_file_type(pdf, Program)),
-  run_process(Program, [File], [program(Program)]).
+  run_process(Program, [File], Opts2).
