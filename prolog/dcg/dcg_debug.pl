626a6af0730bf9c6d6bfcf27aae25d2ca5704f9f
Wouter Beek <me@wouterbeek.com>
2015-10-28 15:28:42 +0100

    DEBUG: Have `dcg_debug.pl' re-export `debug.pl'.

--- prolog/dcg/dcg_debug.pl
+++ prolog/dcg/dcg_debug.pl
@@ -5,17 +5,17 @@
                 % :Dcg_0
   ]
 ).
+:- reexport(library(debug)).
 
 /** <module> DCG debug
 
 DCG-based debug tools.
 
 @author Wouter Beek
-@version 2015/08
+@version 2015/08, 2015/10
 */
 
 :- use_module(library(dcg/dcg_phrase)).
-:- use_module(library(debug)).
 
 :- meta_predicate(dcg_debug(+,//)).
 
