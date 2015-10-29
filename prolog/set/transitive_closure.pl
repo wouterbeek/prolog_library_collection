c2eef54cd3de865608a7e91207447a4039f7ebe8
Wouter Beek <me@wouterbeek.com>
2015-10-29 11:56:15 +0100

    FIXED: Transitive closure CHR rule order.

--- prolog/set/transitive_closure.pl
+++ prolog/set/transitive_closure.pl
@@ -6,13 +6,14 @@
   ]
 ).
 
+:- use_module(library(graph/s/s_graph)).
 :- use_module(library(plunit)).
 :- use_module(library(set/ordset_closure)).
 
 :- chr_constraint(leq/2).
 
-transitivity @ leq(X,Y), leq(Y,Z)  ==> leq(X,Z) .
 idempotence  @ leq(X,Y) \ leq(X,Y) <=> true     .
+transitivity @ leq(X,Y), leq(Y,Z)  ==> leq(X,Z) .
 
 transitive_closure(S1, S2):-
   ordset_closure(S1, S2, transitive_closure).
@@ -24,8 +25,9 @@ test(
   [forall(transitive_closure_test(GName,Closure))]
 ):-
   s_graph_test(GName, G),
-  transitive_closure(G, Closure0),
-  Closure == Closure0.
+  s_graph_components(G, Vs, Es1),
+  transitive_closure(Es1, Es2),
+  s_graph_components(Closure, Vs, Es2).
 
 transitive_closure_test(path(2), [1-[1,2],2-[1,2]]).
 
