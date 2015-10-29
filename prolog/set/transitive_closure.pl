2660dc44c55a99e8d9fd5a81099b1f551345038d
Wouter Beek <me@wouterbeek.com>
2015-10-29 11:44:38 +0100

    DEBUG: Added unit test for `transitive_closure/2'.

--- prolog/set/transitive_closure.pl
+++ prolog/set/transitive_closure.pl
@@ -6,6 +6,7 @@
   ]
 ).
 
+:- use_module(library(plunit)).
 :- use_module(library(set/ordset_closure)).
 
 :- chr_constraint(leq/2).
@@ -15,3 +16,17 @@ idempotence  @ leq(X,Y) \ leq(X,Y) <=> true     .
 
 transitive_closure(S1, S2):-
   ordset_closure(S1, S2, transitive_closure).
+
+:- begin_tests('transitive_closure/2').
+
+test(
+  'transitive_closure(+,-) is det. TRUE',
+  [forall(transitive_closure_test(GName,Closure))]
+):-
+  s_graph_test(GName, G),
+  transitive_closure(G, Closure0),
+  Closure == Closure0.
+
+transitive_closure_test(path(2), [1-[1,2],2-[1,2]]).
+
+:- end_tests('transitive_closure/2').
