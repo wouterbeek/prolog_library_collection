e47c6a6f4e96e366f8abfda53168e811cddffbbe
Wouter Beek <me@wouterbeek.com>
2015-11-04 13:52:29 +0100

    ADDED: `option_pair/2'.

--- prolog/option_ext.pl
+++ prolog/option_ext.pl
@@ -1,25 +1,19 @@
 :- module(
   option_ext,
   [
-    if_option/3 % +Option:compound
-                % +Options:list(compound)
-                % :Goal
+    if_option/3, % +Option:compound
+                 % +Options:list(compound)
+                 % :Goal_0
+    option_pair/2 % ?Option:compound
+                  % ?Pair:pair
   ]
 ).
 :- reexport(library(option)).
 
 /** <module> Option extensions
 
-Translation between options and their constituent parts:
-
-```prolog
-maplist(\Opt^Value^'=..'(Opt,[Name,Value]), Opts, Values)
-```
-
----
-
 @author Wouter Beek
-@version 2015/07
+@version 2015/07, 2015/10
 */
 
 :- meta_predicate(if_option(+,+,0)).
@@ -34,3 +28,12 @@ if_option(Opt, Opts, Goal_0):-
   option(Opt, Opts), !,
   Goal_0.
 if_option(_, _, _).
+
+
+
+%! option_pair(+Option:compound, +Pair:pair) is semidet.
+%! option_pair(+Option:compound, -Pair:pair) is det.
+%! option_pair(-Option:compound, +Pair:pair) is det.
+
+option_pair(Opt, N-V):-
+  Opt =.. [N,V].
