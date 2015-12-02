68082ac91355e4dc1d795e5205ad0ffc6e4346d7
Wouter Beek <me@wouterbeek.com>
2015-12-02 16:21:13 +0100

    ADDED: http_search_pl/3 for IRI query parameters that represent Prolog terms.

--- prolog/http/http_receive.pl
+++ prolog/http/http_receive.pl
@@ -1,9 +1,12 @@
 :- module(
   http_receive,
   [
-    http_search/3 % +Request:list(compound)
-                  % +Key:atom
-                  % -Value:atom
+    http_search/3, % +Request:list(compound)
+                   % +Key:atom
+                   % -Value:atom
+    http_search_pl/3 % +Request:list(compound)
+                     % +Key:atom
+                     % -Value:atom
   ]
 ).
 
@@ -12,7 +15,7 @@
 Support for receiving an HTTP reply.
 
 @author Wouter Beek
-@version 2015/08
+@version 2015/08, 2015/12
 */
 
 
@@ -24,3 +27,11 @@ Support for receiving an HTTP reply.
 http_search(Request, Key, Val):-
   memberchk(search(L), Request),
   memberchk(Key=Val, L).
+
+
+
+%! http_search_pl(+Request:list(compound), +Key:atom, -Value:atom) is det.
+
+http_search_pl(Request, Key, Val):-
+  http_search(Request, Key, Atom),
+  term_to_atom(Val, Atom).
