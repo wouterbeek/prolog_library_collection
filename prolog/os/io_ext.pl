6e3b07a0d5a68c4c17a78c45f567e7cfd5b0e1cb
Wouter Beek <me@wouterbeek.com>
2016-02-02 21:35:48 +0100

    ADDED: write_stream_to_file/3 allowing the written to file to be compressed.

--- prolog/os/io_ext.pl
+++ prolog/os/io_ext.pl
@@ -6,7 +6,8 @@
     read_input_to_codes/2,  % +Input, -Content
     read_input_to_string/2, % +Input, -Content
     write_output_to_atom/2, % :Goal_1, -Content
-    write_stream_to_file/2  % +Read, +File
+    write_stream_to_file/2, % +Read, +File
+    write_stream_to_file/3  % +Read, +File, +Opts
   ]
 ).
 :- reexport(library(readutil)).
@@ -16,7 +17,7 @@
 Predicates that extend the swipl builtin I/O predicates operating on streams.
 
 @author Wouter Beek
-@version 2015/08, 2015/10-2015/11, 2016/01
+@version 2015/08, 2015/10-2015/11, 2016/01-2016/02
 */
 
 :- use_module(library(os/open_any2)).
@@ -37,12 +38,12 @@ Predicates that extend the swipl builtin I/O predicates operating on streams.
 
 %! print_input(+Input, +Opts) is det.
 
-print_input(In, Opts):-
+print_input(In, Opts) :-
   forall(read_input_to_line(In, Cs), print_line(Cs, Opts)).
 
 %! print_line(+Cs, +Opts) is det.
 
-print_line(Cs, Opts):-
+print_line(Cs, Opts) :-
   (option(indent(I), Opts) -> tab(I) ; true),
   format('~s~n', [Cs]).
 
@@ -50,7 +51,7 @@ print_line(Cs, Opts):-
 
 %! read_input_to_atom(+Input, -Atom) is det.
 
-read_input_to_atom(In, A):-
+read_input_to_atom(In, A) :-
   read_input_to_codes(In, Cs),
   atom_codes(A, Cs).
 
@@ -58,7 +59,7 @@ read_input_to_atom(In, A):-
 
 %! read_input_to_codes(+Input, -Cs) is det.
 
-read_input_to_codes(In, Cs):-
+read_input_to_codes(In, Cs) :-
   setup_call_cleanup(
     open_any2(In, read, Read, Close_0, [encoding(utf8)]),
     read_stream_to_codes(Read, Cs),
@@ -69,7 +70,7 @@ read_input_to_codes(In, Cs):-
 
 %! read_input_to_line(+Input, -Cs) is nondet.
 
-read_input_to_line(In, Cs):-
+read_input_to_line(In, Cs) :-
   setup_call_cleanup(
     open_any2(In, read, Read, Close_0, [encoding(utf8)]),
     % NONDET.
@@ -81,7 +82,7 @@ read_input_to_line(In, Cs):-
 
 %! read_input_to_string(+Input, -String) is det.
 
-read_input_to_string(In, S):-
+read_input_to_string(In, S) :-
   read_input_to_codes(In, Cs),
   string_codes(S, Cs).
 
@@ -90,7 +91,7 @@ read_input_to_string(In, S):-
 %! write_output_to_atom(:Goal_1, -Atom) is det.
 % Is called as call(Goal_1, Write).
 
-write_output_to_atom(Goal_1, Atom):-
+write_output_to_atom(Goal_1, Atom) :-
   setup_call_cleanup(
     new_memory_file(Handle),
     (
@@ -107,10 +108,16 @@ write_output_to_atom(Goal_1, Atom):-
 
 
 %! write_stream_to_file(+Read, +File) is det.
+%! write_stream_to_file(+Read, +File, +Opts) is det.
+% Options are passed to open_any2/5.
 
-write_stream_to_file(Read, File):-
+write_stream_to_file(Read, File) :-
+  write_stream_to_file(Read, File, []).
+
+write_stream_to_file(Read, File, Opts1) :-
+  merge_options([type(binary)], Opts1, Opts2),
   setup_call_cleanup(
-    open(File, write, Write, [type(binary)]),
+    open_any2(File, write, Write, Close_0, Opts2),
     copy_stream_data(Read, Write),
-    close(Write)
+    close_any2(Close_0)
   ).
