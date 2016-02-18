ba3c0a45a186e3e78fc72ba3beff992ffc7cbe40
Wouter Beek <me@wouterbeek.com>
2016-02-19 00:16:12 +0100

    FIXED: Return type wrongly documented.

--- prolog/datetime/datetime.pl
+++ prolog/datetime/datetime.pl
@@ -50,7 +50,7 @@ error:has_type(date, time(H,Mi,S)):-
 
 
 
-%! call_time(:Goal_0, -Seconds:nonneg) is det.
+%! call_time(:Goal_0, -Seconds:float) is det.
 
 call_time(Goal_0, N):- get_time(X), Goal_0, get_time(Y), N is Y - X.
 
