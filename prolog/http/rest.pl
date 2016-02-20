205ce75577c47a0c1b9ce79f03b46a79d107bfbd
Wouter Beek <me@wouterbeek.com>
2016-02-20 14:12:51 +0000

    ENHANCED: Use builtins to determine whether a singular or plural REST call has to be made.

--- prolog/http/rest.pl
+++ prolog/http/rest.pl
@@ -1,7 +1,7 @@
 :- module(
   rest,
   [
-    rest_handler/5,   % +Request, +HandleId, :Exists_1,   :Singular_3, :Plural_2
+    rest_handler/5,   % +Request, +HandleId, :Exists_1, :Singular_3, :Plural_2
     rest_mediatype/3, % +Method, +MediaTypes, :Plural_2
     rest_mediatype/4  % +Method, +MediaTypes, +Resource, :Singular_3
   ]
@@ -13,14 +13,9 @@
 @version 2016/02
 */
 
-:- use_module(library(atom_ext)).
-:- use_module(library(http/html_write)).
-:- use_module(library(http/http_dispatch)).
-:- use_module(library(http/http_header)).
+:- use_module(library(http/html_write)). % HTML meta.
 :- use_module(library(http/http_receive)).
-:- use_module(library(settings)).
-
-:- setting(rest:host, atom, '', 'The host IRI with respect to which the REST API should work.').
+:- use_module(library(uri/uri_ext)).
 
 :- html_meta
    rest_handler(+, +, 1, 3, 2),
@@ -32,23 +27,17 @@
 rest_handler(Req, HandleId, Exists_1, Singular_3, Plural_2) :-
   memberchk(request_uri(Local), Req),
   http_accept(Req, MTs),
-  http_link_to_id(HandleId, [], Endpoint),
+  http_link_to_id(HandleId, Endpoint),
   http_method(Req, Method),
-  (   same_iri(Local, Endpoint)
+  (   Local == Endpoint
   ->  call(Plural_2, Method, MTs)
-  ;   setting(rest:host, Host),
-      atom_concat(Host, Local, Res),
+  ;   uri_path(Res, Local),
       call(Exists_1, Res)
   ->  call(Singular_3, Method, MTs, Res)
   ), !.
 rest_handler(Req, _, _, _, _) :-
   memberchk(request_uri(Res), Req),
   http_status_reply(Req, not_found(Res)).
-
-same_iri(X1, Y1):-
-  atom_ending_in(X1, /, X2),
-  atom_ending_in(Y1, /, Y2),
-  X2 == Y2.
 
 
 rest_mediatype(Method, [], _) :- !, http_status_reply(Method, non_acceptable(_)).
