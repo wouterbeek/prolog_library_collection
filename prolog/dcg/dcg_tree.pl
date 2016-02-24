548ee40afc01b533e09ed32fcd80d20ab04b82e0
Wouter Beek <me@wouterbeek.com>
2016-02-25 00:06:58 +0100

    ENHANCED: Improved API for dcg_tree.  Node writer is now an option, allowing more flexibility for writing the tree.

--- prolog/dcg/dcg_tree.pl
+++ prolog/dcg/dcg_tree.pl
@@ -2,8 +2,7 @@
   dcg_tree,
   [
     dcg_tree//1, % +Tree
-    dcg_tree//2, % :Node_5, +Tree
-    dcg_tree//3  % :Node_5, +Tree, +Opts
+    dcg_tree//2  % +Tree, :Opts
   ]
 ).
 
@@ -17,69 +16,49 @@
 :- use_module(library(dcg/dcg_content)).
 :- use_module(library(option)).
 
+is_meta(node_writer).
+
 :- meta_predicate
-    dcg_node0(+, 5, +, +, +, ?, ?),
-    dcg_tree(5, +, ?, ?),
-    dcg_tree(5, +, +, ?, ?),
-    dcg_tree0(+, 5, +, +, ?, ?),
-    dcg_trees0(+, 5, +, +, ?, ?).
-
-
+    dcg_tree(+, :, ?, ?).
 
 
 
 %! dcg_tree(+Tree)// is det.
-%! dcg_tree(:Node_5, +Tree)// is det.
-%! dcg_tree(:Node_5, +Tree, +Opts)// is det.
+%! dcg_tree(+Tree, +Opts)// is det.
 % The following options are supported:
-%   * indent(+nonneg)
+%   * node_writer(+callable)
 
 dcg_tree(Tree) -->
-  dcg_tree(default_node, Tree).
+  dcg_tree(Tree, []).
 
-dcg_tree(Node_5, Tree) -->
-  dcg_tree(Node_5, Tree, []).
-
-dcg_tree(Node_5, Tree, Opts) -->
-  {option(indent(I), Opts, 0)},
-  dcg_tree0(I, Node_5, Tree, []).
-
-default_node(Node, _, _) --> atom(Node).
+dcg_tree(Tree, Opts1) -->
+  {meta_options(is_meta, Opts1, Opts2)},
+  dcg_tree0(Tree, [], Opts2).
 
 
-%! dcg_tree0(+Indent:nonneg, :Node_5, +Tree, +InvPath)// is det.
+%! dcg_tree0(+Tree, +InvPath, +Opts)// is det.
 
-dcg_tree0(I1, Node_5, t(Node,Trees), InvPath) -->
+dcg_tree0(t(Node,Trees), InvPath, Opts) -->
   {(Trees = [] -> Leaf = true ; Leaf = false)},
-  dcg_node0(I1, Node_5, Node, InvPath, Leaf),
-  (   {Leaf == true}
-  ->  ""
-  ;   {I2 is I1 + 1},
-      dcg_trees0(I2, Node_5, Trees, [Node|InvPath])
-  ).
+  {option(node_writer(NodeWriter_4), Opts, default_node_writer)},
+  dcg_call(NodeWriter_4, [Node|InvPath], Leaf), nl,
+  ({Leaf == true} -> "" ; dcg_trees0(Trees, [Node|InvPath], Opts)).
 
 
-%! dcg_trees0(+Indent:nonneg, :Node_5, +Trees:list, +InvPath)// is det.
+%! dcg_trees0(+Trees:list, +InvPath, +Opts)// is det.
 
-dcg_trees0(I, Node_5, [H|T], InvPath) -->
-  dcg_tree0(I, Node_5, H, InvPath),
-  (   {T == []}
-  ->  ""
-  ;   dcg_trees0(I, Node_5, T, InvPath)
-  ).
+dcg_trees0([H|T], InvPath, Opts) -->
+  dcg_tree0(H, InvPath, Opts),
+  ({T == []} ->  "" ; dcg_trees0(T, InvPath, Opts)).
 
 
-%! dcg_node0(+Indent:nonneg, :Node_5, +Node, +InvPath, +Leaf)// is det.
+default_node_writer([Node|InvPath], _) -->
+  indent_path(InvPath),
+  atom(Node), nl.
 
-dcg_node0(I1, Node_5, Node, InvPath, Leaf) -->
-  (   {I1 =:= 0}
-  ->  ""
-  ;   {I2 is I1 - 1},
-      tabby(I2),
-      "├ "
-  ),
-  dcg_once(dcg_call(Node_5, Node, InvPath, Leaf)),
-  nl.
+indent_path(Path) -->
+  {length(Path, Len)},
+  ({Len =:= 0} -> "" ; {Tabs is Len - 1}, tabby(Tabs), "├ ").
 
-tabby(0) --> !, "".
+tabby(0)  --> !, "".
 tabby(N1) --> "│ ", {N2 is N1 - 1}, tabby(N2).
