7be512f5c7b3d5b19fc9cd87ea7937ad16e51bf6
Wouter Beek <me@wouterbeek.com>
2016-02-29 19:34:59 +0100

    ADDED: print_tree/[1,2].

--- prolog/tree/s_tree.pl
+++ prolog/tree/s_tree.pl
@@ -1,12 +1,14 @@
 :- module(
   s_tree,
   [
-    paths_to_trees/2,         % +AllSubPaths:list(list), -Tree
     edges_to_root/2,          % +Es, -Root
     edges_to_tree/2,          % +Es, -Tree
     is_s_tree/1,              % @Term
+    paths_to_trees/2,         % +AllSubPaths:list(list), -Tree
+    print_tree/1,             % +Tree
+    print_tree/2,             % +Tree, :Opts
     some_subpaths_to_trees/2, % +SomeSubPaths:list(list), -Tree
-    s_tree_to_graph/2,        % +Tree, -Graph
+    tree_to_graph/2,          % +Tree, -Graph
     tree_depth/2,             % +Tree, -Depth
     tree_to_leaf_coord/2      % +Tree, -Coord:list(nonneg)
   ]
@@ -48,10 +50,60 @@ a
 :- use_module(library(dcg/dcg_tree)).
 :- use_module(library(graph/s/s_edge)).
 :- use_module(library(list_ext)).
+:- use_module(library(option)).
 :- use_module(library(ordsets)).
 :- use_module(library(pairs)).
 
+:- meta_predicate
+    print_tree(+, :).
 
+is_meta(node_writer).
+
+:- predicate_options(print_tree/2, 2, [
+     output(+stream),
+     pass_to(dcg_tree//2, 2)
+   ]).
+
+
+
+
+
+%! edges_to_root(+Edges:list(compound), -Root:term) is semidet.
+
+edges_to_root(Es, Root):-
+  member(Root-_, Es),
+  \+ member(_-Root, Es),
+  % The root is unique.
+  \+ (
+    member(SecondRoot-_, Es),
+    \+ member(_-SecondRoot, Es),
+    SecondRoot \== Root
+  ), !.
+
+
+
+%! edges_to_tree(+Edges:list(compound), -Tree:compound) is det.
+
+edges_to_tree([], []):- !.
+edges_to_tree(Es, Tree):-
+  edges_to_root(Es, Root),
+  edges_to_tree(Root, Tree, Es, []).
+
+edges_to_tree(Parent, t(Parent,SubTrees), Es1, Es3):-
+  edges_to_children(Parent, Children, Es1, Es2),
+  foldl(edges_to_tree, Children, SubTrees, Es2, Es3).
+
+edges_to_children(Parent, [H|T], Es1, Es3):-
+  selectchk(edge(Parent,_,H), Es1, Es2), !,
+  edges_to_children(Parent, T, Es2, Es3).
+edges_to_children(_, [], Es, Es).
+
+
+
+%! is_s_tree(@Term) is semidet.
+
+is_s_tree(t(_,L)) :-
+  forall(member(T, L), is_s_tree(T)).
 
 
 
@@ -102,55 +154,16 @@ paths_to_trees(Ls1, Trees):-
 
 
 
-%! edges_to_root(+Edges:list(compound), -Root:term) is semidet.
+%! print_tree(+Tree) is det.
+%! print_tree(+Tree, +Opts) is det.
 
-edges_to_root(Es, Root):-
-  member(Root-_, Es),
-  \+ member(_-Root, Es),
-  % The root is unique.
-  \+ (
-    member(SecondRoot-_, Es),
-    \+ member(_-SecondRoot, Es),
-    SecondRoot \== Root
-  ), !.
+print_tree(Tree) :-
+  print_tree(Tree, []).
 
-
-
-%! edges_to_tree(+Edges:list(compound), -Tree:compound) is det.
-
-edges_to_tree([], []):- !.
-edges_to_tree(Es, Tree):-
-  edges_to_root(Es, Root),
-  edges_to_tree(Root, Tree, Es, []).
-
-edges_to_tree(Parent, t(Parent,SubTrees), Es1, Es3):-
-  edges_to_children(Parent, Children, Es1, Es2),
-  foldl(edges_to_tree, Children, SubTrees, Es2, Es3).
-
-edges_to_children(Parent, [H|T], Es1, Es3):-
-  selectchk(edge(Parent,_,H), Es1, Es2), !,
-  edges_to_children(Parent, T, Es2, Es3).
-edges_to_children(_, [], Es, Es).
-
-
-
-%! is_s_tree(@Term) is semidet.
-
-is_s_tree(t(_,L)) :-
-  forall(member(T, L), is_s_tree(T)).
-
-
-
-%! s_tree_to_graph(+Tree, -Graph) is det.
-
-s_tree_to_graph(Tree, Graph) :-
-  s_tree_to_edges(Tree, Es),
-  s_edges_vertices(Es, Vs),
-  Graph = graph(Vs, Es).
-
-s_tree_to_edges(t(X,Ts), Es) :-
-  findall([X-Y|Es], (member(t(Y,Ts0), Ts), s_tree_to_edges(t(Y,Ts0), Es)), Ess),
-  append(Ess, Es).
+print_tree(Tree, Opts1) :-
+  meta_options(is_meta, Opts1, Opts2),
+  option(output(Sink), Opts2, current_output),
+  dcg_with_output_to(Sink, dcg_tree(Tree, Opts2)).
 
 
 
@@ -165,6 +178,19 @@ some_subpaths_to_trees(SomeSubPaths, Trees):-
     AllSubPaths
   ),
   paths_to_trees(AllSubPaths, Trees).
+
+
+
+%! tree_to_graph(+Tree, -Graph) is det.
+
+tree_to_graph(Tree, Graph) :-
+  tree_to_edges(Tree, Es),
+  s_edges_vertices(Es, Vs),
+  Graph = graph(Vs, Es).
+
+tree_to_edges(t(X,Ts), Es) :-
+  findall([X-Y|Es], (member(t(Y,Ts0), Ts), tree_to_edges(t(Y,Ts0), Es)), Ess),
+  append(Ess, Es).
 
 
 
