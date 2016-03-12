bf434c71b8ffaabc991ddb15e85cc8d919c6439b
Wouter Beek <me@wouterbeek.com>
2016-03-12 07:48:18 +0100

    CLEAN: Simple cleanup of print_ext.

--- prolog/print_ext.pl
+++ prolog/print_ext.pl
@@ -1,15 +1,12 @@
 :- module(
   print_ext,
   [
-    formatln/1, % +Format:atom
-    formatln/2, % +Format:atom
-                % +Arguments:list
-    formatln/3, % +Write:stream
-                % +Format:atom
-                % +Arguments:list
-    print_table/1, % +Rows:list
-    print_table/2, % +Rows:list
-                   % :Options:list(compound)
+    formatln/1,    % +Format
+    formatln/2,    % +Format, +Args
+    formatln/3,    % +Write, +Format, +Args
+    indent/1,      % +NoSpaces
+    print_table/1, % +Rows
+    print_table/2, % +Rows, :Opts
     tab/0
   ]
 ).
@@ -19,14 +16,15 @@
 Additional predicates for printing.
 
 @author Wouter Beek
-@version 2015/08, 2015/11
+@version 2015/08, 2015/11, 2016/03
 */
 
 :- use_module(library(dcg/dcg_ext)).
 :- use_module(library(dcg/dcg_table)).
 :- use_module(library(settings)).
 
-:- meta_predicate(print_table(+,:)).
+:- meta_predicate
+    print_table(+, :).
 
 :- predicate_options(print_table/2, 2, [
      pass_to(dcg_table/4, 2)
@@ -40,33 +38,38 @@ Additional predicates for printing.
 
 
 
-%! formatln(+Format:atom) is det.
-% Variant of format/1 with a newline appended.
+%! formatln(+Format) is det.
+%! formatln(+Format, +Args) is det.
+%! formatln(+Write, +Format, +Args) is det.
+% Variants of format/[1-3] with a newline appended.
 
-formatln(Format):- format(Format), nl.
+formatln(Format):-
+  format(Format), nl.
 
+formatln(Format, Args):-
+  format(Format, Args), nl.
 
-%! formatln(+Format:atom, +Arguments:list) is det.
-% Variant of format/2 with a newline appended.
-
-formatln(Format, Args):- format(Format, Args), nl.
-
-
-%! formatln(+Write:stream, +Format:atom, +Arguments:list) is det.
-% Variant of format/3 with a newline appended.
-
-formatln(Write, Format, Args):- format(Write, Format, Args), nl(Write).
+formatln(Write, Format, Args):-
+  format(Write, Format, Args),
+  nl(Write).
 
 
 
-%! print_table(+Rows:list) is det.
-% Wrapper around print_table/2 with default options.
+%! indent(+NoSpaces) is det.
+
+indent(N) :-
+  N < 1, !.
+indent(N1) :-
+  write(" "),
+  N2 is N1 - 1,
+  indent(N2).
+
+
+%! print_table(+Rows) is det.
+%! print_table(+Rows, :Opts) is det.
 
 print_table(L):-
   print_table(_, L).
-
-
-%! print_table(+Rows:list, :Options:list(compound)) is det.
 
 print_table(Rows, Opts):-
   dcg_with_output_to(current_output, dcg_table(Rows, Opts)).
