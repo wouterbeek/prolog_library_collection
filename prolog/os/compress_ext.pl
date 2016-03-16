4b50339fe97ee740fc4112540b2d0aa8a81d06cf
Wouter Beek <me@wouterbeek.com>
2016-03-16 10:10:20 +0100

    ADDED: compress_file/2, when in- and output file are the same.

--- prolog/os/compress_ext.pl
+++ prolog/os/compress_ext.pl
@@ -1,23 +1,36 @@
 :- module(
   compress_ext,
   [
-    compress_file/3 % +From, +Compress:oneof([deflate,gzip,none]), +To
+    compress_file/2, % +From, +Compress
+    compress_file/3  % +From, +Compress, +To
   ]
 ).
 
 /** <module> Compress extension
 
 @author Wouter Beek
-@version 2016/01
+@version 2016/01, 2016/03
 */
 
+:- use_module(library(os/file_ext)).
 :- use_module(library(zlib)).
 
 
 
 
 
-%! compress_file(+From, +Compress:oneof([deflate,gzip,none]), +To) is det.
+%! compress_file(+From, +Compress) is det.
+%! compress_file(+From, +Compress, +To) is det.
+% The following values are supported for Compress:
+%   - `deflate’
+%   - `gzip’
+%   - `none’
+
+compress_file(From, Compress) :-
+  thread_file(From, Tmp),
+  compress_file(From, Compress, Tmp),
+  rename_file(Tmp, From).
+
 
 compress_file(From, none, To) :- !,
   rename_file(From, To).
