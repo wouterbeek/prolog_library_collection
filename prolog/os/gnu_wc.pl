41a721b1df9f5a01fe480a0837d1f0b2aea8eba7
Wouter Beek <me@wouterbeek.com>
2016-03-30 09:19:24 +0200

    CLEAN: Reuse rdf11 for XML DOM to atom writing. CHANGED: Reimplemented GNU wc API to be somewhat more generic.

--- prolog/os/gnu_wc.pl
+++ prolog/os/gnu_wc.pl
@@ -1,7 +1,8 @@
 :- module(
   gnu_wc,
   [
-    file_lines/2 % +File, -NoLines
+    file_lines/2, % +File, -NunLines
+    wc/4          % +File, -NumLines, -NumWords, -NumBytes
   ]
 ).
 
@@ -11,7 +12,7 @@
 @version 2015/08, 2015/10, 2016/03
 */
 
-:- use_module(library(dcg/dcg_ext)).
+:- use_module(library(dcg/basics)).
 :- use_module(library(os/io_ext)).
 :- use_module(library(os/process_ext)).
 
@@ -20,11 +21,26 @@
 
 
 %! file_lines(+File, -NoLines) is det.
-% @tbd Use phrase_from_stream/2.
 
 file_lines(File, N):-
-  run_process(wc, ['-l',file(File)], [output_goal(parse_wc(N))]).
+  wc(File, N, _, _).
 
-parse_wc(N, Read):-
+
+
+%! wc(+File, -NumLines, -NumWords, -NumBytes) is det.
+
+wc(File, N1, N2, N3) :-
+  run_process(wc, [file(File)], [output_goal(parse_wc0(N1,N2,N3))]).
+
+parse_wc0(N1, N2, N3, Read):-
   read_input_to_codes(Read, Cs),
-  phrase((integer(N), done), Cs).
+  phrase(wc0(N1, N2, N3), Cs, _).
+
+% Example:
+% ```bash
+%    427  1818 13512 README.md
+% ```
+wc0(N1, N2, N3) -->
+  whites, integer(N1),
+  whites, integer(N2),
+  whites, integer(N3).
--- prolog/xml/xml_dom.pl
+++ prolog/xml/xml_dom.pl
@@ -1,44 +1,31 @@
 :- module(
   xml_dom,
   [
-    atom_to_xml_dom/2, % +Atom:atom
-                       % -Dom:list(compound)
-    xml_dom_as_atom//1, % +Dom:list(compound)
-    xml_dom_to_atom/2, % +Dom, -Atom
-    xml_dom_to_atom/3, % +Dom:list(compound)
-                       % -Atom:atom
-                       % +Options:list(compound)
-    xml_serve_dom/2 % +Dom:list(compound)
-                    % +Options:list(compound)
+    atom_to_xml_dom/2,  % +Atom, -Dom
+    xml_dom_as_atom//1, % +Dom
+    xml_serve_dom/1     % +Dom
   ]
 ).
 
 /** <module> XML DOM
 
 @author Wouter Beek
-@license MIT license
-@version 2015/07, 2015/10
+@version 2015/07, 2015/10, 2016/03
 */
 
 :- use_module(library(http/html_write)).
 :- use_module(library(http/http_path)).
 :- use_module(library(http/http_request)).
-:- use_module(library(lists)).
 :- use_module(library(memfile)).
-:- use_module(library(option)).
+:- use_module(library(semweb/rdf11), []).
 :- use_module(library(sgml)).
 :- use_module(library(sgml_write)).
 
-:- predicate_options(xml_dom_to_atom/3, 3, [
-     style(+atom),
-     pass_to(xml_write/3, 3)
-   ]).
 
 
 
 
-
-%! atom_to_xml_dom(+Atom:atom, Dom:compound) is det.
+%! atom_to_xml_dom(+Atom, Dom) is det.
 
 atom_to_xml_dom(A, Dom):-
   setup_call_cleanup(
@@ -53,87 +40,24 @@ atom_to_xml_dom(A, Dom):-
 
 
 
-%! stylesheet_pi(+Spec:compound, -Pi:compound) is det.
-% Wrapper around stylesheet_pi/3 using MIME `text/css`.
-
-stylesheet_pi(Spec, Pi):-
-  stylesheet_pi('text/css', Spec, Pi).
-
-%! stylesheet_pi(+Mine:atom, +Spec:compound, -Pi:compound) is det.
-
-stylesheet_pi(Mime, Spec, pi(Pi)):-
-  http_absolute_location(Spec, Loc, []),
-  format(atom(Pi), 'xml-stylesheet type="~w" href="~w"', [Mime,Loc]).
-
-
-
-%! xml_dom_as_atom(+Dom:list(compound))// is det.
+%! xml_dom_as_atom(+Dom)// is det.
 % Includes the given DOM inside the generated HTML page.
 %
 % DOM is either a list or compound term or an atom.
 
 xml_dom_as_atom(Dom) -->
-  {xml_dom_to_atom(Dom, A)},
+  {rdf11:in_xml_literal(xml, Dom, A)},
   html(\[A]).
 
 
 
-%! xml_dom_to_atom(+Dom:list(compound), -Xml:atom) is det.
-% Wrapper around xml_dom_to_atom/3.
-
-xml_dom_to_atom(Dom, A):-
-  xml_dom_to_atom(Dom, A, []).
-
-
-%! xml_dom_to_atom(
-%!   +Dom:list(compound),
-%!   -Xml:atom,
-%!   +Options:list(compound)
-%! ) is det.
-% The following options are supported:
-%   * dtd(+atom)
-%     The atomic name of the DTD that should be used for the XML DOM.
-%     The DTD is first searched for in the cache of DTD objects.
-%     If the given doctype has no associated DTD in the cache,
-%     it searches for a file using the file search path =dtd=.
-%   * style(+atom)
-%     The atomic name of a style file on the =css= search path.
-
-xml_dom_to_atom(Dom, A, Opts1):-
-  % Add style to XML DOM.
-  (   select_option(style(Base), Opts1, Opts2)
-  ->  file_name_extension(Base, css, LocalName),
-      stylesheet_pi(css(LocalName), Pi),
-      Dom0 = [Pi|Dom]
-  ;   Dom0 = Dom,
-      Opts2 = Opts1
-  ),
-
-  % Set the header to false, since this XML content will be inserted inside
-  % a Web page.
-  % We do add the stylesheet parsing instruction, as this is allowed
-  % in -- at least -- Firefox.
-  merge_options([header(false)], Opts2, Opts3),
-  with_output_to(atom(A), xml_write(Dom0, Opts3)).
-
-
-
-%! xml_serve_dom(+Dom:list(compound), +Options:list(compound)) is det.
+%! xml_serve_dom(+Dom) is det.
 % Serves the given XML DOM.
-%
-% The following options are supported:
-%   * `dtd(+Doctype:atom)`
-%     The atomic name of the DTD that should be used for the XML DOM.
-%     The DTD is first searched for in the cache of DTD objects.
-%     If the given doctype has no associated DTD in the cache,
-%     it searches for a file using the file search path =dtd=.
-%   * `style(+StyleName:atom)`
-%     The atomic name of a style file on the =css= search path.
 
-xml_serve_dom(Dom, Options):-
-  xml_dom_to_atom(Dom, A, Options),
+xml_serve_dom(Dom) :-
+  rdf11:in_xml_literal(xml, Dom, A),
   % The User Agent needs to know the content type and encoding.
   % If the UTF-8 encoding is not given here explicitly,
   % Prolog throws an IO exception on `format(XML)`.
-  format('Content-type: application/xml; charset=utf-8~n~n'),
+  format("Content-type: application/xml; charset=utf-8~n~n"),
   format(A).
