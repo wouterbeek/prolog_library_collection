3003a57f1db7114634b2851ba1efb316168e9720
Wouter Beek <me@wouterbeek.com>
2016-05-03 21:36:15 +0200

    typo

--- pack.pl
+++ pack.pl
@@ -2,7 +2,7 @@ author('Wouter Beek', 'me@wouterbeek.com').
 download('https://github.com/wouterbeek/Prolog_Library_Collection/release/*.zip').
 home('https://github.com/wouterbeek/Prolog_Library_Collection').
 maintainer('Wouter Beek', 'me@wouterbeek.com').
-name('Prolog_Library_Collection').
+name('Prolog-Library-Collection').
 packager('Wouter Beek', 'me@wouterbeek.com').
 title('A collection of SWI-Prolog libraries.').
 version('0.0.1').
