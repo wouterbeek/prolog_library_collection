9fd1cf6782b2474a512129ae91a540afb2c911e2
Wouter Beek <me@wouterbeek.com>
2016-06-13 20:44:16 +0200

    tmp

---
+++ tmp/file_entry.pl
@@ -0,0 +1,57 @@
+:- module(
+  file_entry,
+  [
+    dir_file_entry/2,      % +Dir, -FileEntry
+    file_entry_file/3,     % +FileEntry, +Ext, -File
+    file_entry_extension/2 % +FileEntry, ?Ext
+  ]
+).
+
+/** <module> File entry
+
+Support for archive entries inside data files.  For many purposes we
+want to abstract away from whether a data file is archive or not;
+compressed or not.
+
+@author Wouter Beek
+@version 2016/05
+*/
+
+:- use_module(library(os/archive_ext)).
+:- use_module(library(os/dir_ext)).
+:- use_module(library(os/file_ext)).
+:- use_module(library(os/open_any2)).
+:- use_module(library(yall)).
+
+
+
+
+
+%! dir_file_entry(+Dir, -FileEntry) is nondet.
+
+dir_file_entry(Dir, file_entry(File,Entry)) :-
+  dir_file_recursive(Dir, File),
+  call_on_stream(File, [_,M,M]>>true, [entry_name(Entry)]).
+
+
+
+%! file_entry_file(+FileEntry, +Ext, -File) is det.
+
+file_entry_file(file_entry(File1,Entry), Ext, File2) :-
+  relative_file_name(File0, File1, Entry),
+  file_change_extension(File0, Ext, File2),
+  create_file_directory(File2).
+
+
+
+%! file_entry_extension(+FileEntry, +Ext) is semidet.
+%! file_entry_extension(+FileEntry, -Ext) is semidet.
+%
+% Heuristically determine the Ext of data that is stored in
+% FileEntry.
+
+file_entry_extension(file_entry(File,Entry), Ext) :-
+  is_archive_file_name(File), !,
+  file_extension(Entry, Ext).
+file_entry_extension(file_entry(File,_), Ext) :-
+  file_extension(File, Ext).
