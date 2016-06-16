9d3926a8f6da6e134b75d61eacb404ba3301284c
Wouter Beek <me@wouterbeek.com>
2016-06-16 08:42:59 +0200

    ADDED: Wrapper http_output/1 @http_ext

--- prolog/http/http_ext.pl
+++ prolog/http/http_ext.pl
@@ -10,6 +10,7 @@
     http_iri/2,               % +Req, -iri
     http_link_to_id/2,        % +HandleId, -Local
     http_method/2,            % +Req, -Method
+    http_output/1,            % -Output
     http_output/2,            % +Req, -Output
     http_query/1,             % -D
     http_query/2,             % +Req, -D
@@ -119,7 +120,13 @@ http_method(Req, M) :-
 
 
 
+%! http_output(-Output) is det.
 %! http_output(+Req, -Output) is det.
+
+http_output(Out) :-
+  http_current_request(Req),
+  http_output(Req, Out).
+
 
 http_output(Req, Out) :-
   memberchk(pool(client(_,_,_,Out)), Req).
