13a1c114fb508227a58ef2515785575af821f682
Wouter Beek <me@wouterbeek.com>
2016-07-04 00:22:25 +0200

    ENHANCED: Ability to set results key in pagination to a custom name, supporting the GeoJSON use case @pagination

--- prolog/pagination.pl
+++ prolog/pagination.pl
@@ -10,7 +10,7 @@
 /** <module> Pagination
 
 @author Wouter Beek
-@verson 2016/03, 2016/05-2016/06
+@verson 2016/03, 2016/05-2016/07
 */
 
 :- use_module(library(dict_ext)).
@@ -34,11 +34,15 @@
 %
 %   * page(+nonneg) Default is 1.
 %
+%   * results_key(+atom) Allows the results key to be set.  This is
+%   used to generate GeoJSON, where the results key must be
+%   `features`.  Default is `results`.
+%
 % The following keys are in Result:
-%   - number_of_results
-%   - page
-%   - page_size
-%   - results
+%   * number_of_results
+%   * page
+%   * page_size
+%   * results
 
 pagination(Pattern, Goal_0, Result) :-
   pagination(Pattern, Goal_0, _{}, Result).
@@ -48,17 +52,18 @@ pagination(Pattern, Goal_0, Opts1, Result) :-
   mod_dict(page_size, Opts1, 100, PageSize, Opts2),
   mod_dict(page, Opts2, 1, StartPage, Opts3),
   put_dict(page0, Opts3, 0, Opts4),
+  mod_dict(results_key, Opts4, results, ResultsKey, Opts5),
   % NONDET
   findnsols(PageSize, Pattern, Goal_0, Results),
   length(Results, LenResults),
   dict_inc(page0, Opts4),
   (Opts4.page0 >= StartPage -> true ; false),
-  Result = _{
-    number_of_results: LenResults,
-    page: Opts4.page0,
-    page_size: PageSize,
-    results: Results
-  }.
+  dict_pairs(Result, [
+    number_of_results-LenResults,
+    page-Opts5.page0,
+    page_size-PageSize,
+    ResultsKey-Results
+  ]).
 
 
 
