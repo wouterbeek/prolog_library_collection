04bb56f0b4368bc5370b21a46f4c73912e85902c
Wouter Beek <me@wouterbeek.com>
2016-07-16 08:53:13 +0200

    ADDED: Rewrote simplified API for hashes: md5/[2,3] and sha/[2,3] @has_ext ADDED: Convenience predicates for when two sinks share the same options: call_onto_streams/6 and call_to_stream/4 @io CLEAN: Renamed option arguments to make clear whether they are for the source or sink (first or second) @io

--- prolog/hash_ext.pl
+++ prolog/hash_ext.pl
@@ -1,7 +1,10 @@
 :- module(
   hash_ext,
   [
-    md5/2 % @Term, -Hash
+    md5/2, % +Term, -Hash
+    md5/3, % +Term, -Hash, +Opts
+    sha/2, % +Term, -Hash
+    sha/3  % +Term, -Hash, +Opts
   ]
 ).
 
@@ -10,15 +13,36 @@
 Extended support for using hashes.
 
 @author Wouter Beek
-@version 2015/08, 2016/01
+@version 2016/07
 */
 
-:- use_module(library(semweb/rdf_db), [rdf_atom_md5/3]).
+:- use_module(library(md5)).
+:- use_module(library(sha)).
 
 
 
-%! md5(@Term, -Hash:atom) is det.
 
-md5(Term, Hash):-
-  term_to_atom(Term, Atom),
-  rdf_atom_md5(Atom, 1, Hash).
+
+%! md5(+Term, -Hash) is det.
+%! md5(+Term, -Hash, +Opts) is det.
+
+md5(Term, Hash) :-
+  md5(Term, Hash, []).
+
+
+md5(Term, Hash, Opts) :-
+  term_to_atom(Term, A),
+  md5_hash(A, Hash, Opts).
+
+
+
+%! sha(+Term, -Hash) is det.
+%! sha(+Term, -Hash, +Opts) is det.
+
+sha(Data, Hash) :-
+  sha(Data, Hash, []).
+
+
+sha(Term, Hash, Opts) :-
+  term_to_atom(Term, A),
+  sha(A, Hash, Opts).
--- prolog/os/io.pl
+++ prolog/os/io.pl
@@ -2,17 +2,19 @@
   io,
   [
     call_on_stream/2,        % +Source, :Goal_3
-    call_on_stream/3,        % +Source, :Goal_3, +Opts
+    call_on_stream/3,        % +Source, :Goal_3, +SourceOpts
     call_onto_stream/3,      % +Source, +Sink, :Goal_4
-    call_onto_stream/5,      % +Source, +Sink, :Goal_4, +Opts1, +Opts2
+    call_onto_stream/5,      % +Source, +Sink, :Goal_4, +SourceOpts, +SinkOpts
     call_onto_streams/4,     % +Source, +Sink1, +Sink2, :Goal_5
-    call_onto_streams/7,     % +Source, +Sink1, +Sink2, :Goal_5, +InOpts, +OutOpts1, +OutOpts2
+    call_onto_streams/6,     % +Source, +Sink1, +Sink2, :Goal_5, +SourceOpts, +SinkOpts
+    call_onto_streams/7,     % +Source, +Sink1, +Sink2, :Goal_5, +SourceOpts, +Sink1Opts, +Sink2Opts
     call_to_atom/2,          % :Goal_1, -A
     call_to_codes/2,         % :Goal_1, -Cs
     call_to_stream/2,        % +Sink, :Goal_1
-    call_to_stream/3,        % +Sink, :Goal_1, +Opts
+    call_to_stream/3,        % +Sink, :Goal_1, +SinkOpts
     call_to_streams/3,       % +Sink1, +Sink2, :Goal_2
-    call_to_streams/5,       % +Sink1, +Sink2, :Goal_2, +Opts1, +Opts2
+    call_to_streams/4,       % +Sink1, +Sink2, :Goal_2, +SinkOpts
+    call_to_streams/5,       % +Sink1, +Sink2, :Goal_2, +Sink1Opts, +Sink2Opts
     call_to_string/2,        % :Goal_1, -Str
     close_any/2,             % +Close, -Meta
     close_any/3,             % +Close, +Meta1, -Meta2
@@ -44,6 +46,7 @@
     call_onto_stream(+, +, 4, +, +),
     call_onto_stream0(+, 4, +, +, +, -),
     call_onto_streams(+, +, +, 5),
+    call_onto_streams(+, +, +, 5, +, +),
     call_onto_streams(+, +, +, 5, +, +, +),
     call_onto_streams0(+, +, 5, +, +, +, +, -),
     call_to_atom(1, -),
@@ -51,6 +54,7 @@
     call_to_stream(+, 1),
     call_to_stream(+, 1, +),
     call_to_streams(+, +, 2),
+    call_to_streams(+, +, 2, +),
     call_to_streams(+, +, 2, +, +),
     call_to_string(1, +).
 
@@ -59,7 +63,7 @@
 
 
 %! call_on_stream(+Source, :Goal_3) is det.
-%! call_on_stream(+Source, :Goal_3, +Opts) is det.
+%! call_on_stream(+Source, :Goal_3, +SourceOpts) is det.
 %
 % The following call is made: `call(Goal_3, In)`.
 %
@@ -79,15 +83,15 @@ call_on_stream(Source, Goal_3) :-
   call_on_stream(Source, Goal_3, []).
 
 
-call_on_stream(Source, Goal_3, Opts) :-
+call_on_stream(Source, Goal_3, SourceOpts) :-
   % This allows the calling context to request one specific entity.
-  option(entry_name(EntryName), Opts, _),
+  option(entry_name(EntryName), SourceOpts, _),
   % Archive options that cannot be overridden.
   findall(format(Format), archive_format(Format, true), FormatOpts),
-  merge_options([close_parent(false)|FormatOpts], Opts, ArchOpts1),
+  merge_options([close_parent(false)|FormatOpts], SourceOpts, ArchOpts1),
   % Archive options that can be overridden.
   merge_options(ArchOpts1, [filter(all)], ArchOpts2),
-  merge_options([metadata(Meta1)], Opts, OpenOpts),
+  merge_options([metadata(Meta1)], SourceOpts, OpenOpts),
   setup_call_cleanup(
     open_any(Source, read, In1, Close, OpenOpts),
     setup_call_cleanup(
@@ -100,27 +104,27 @@ call_on_stream(Source, Goal_3, Opts) :-
         ),
         % Semi-deterministic if an archive entry name is given.
         (   nonvar(EntryName)
-        ->  once(call_on_stream0(Arch, EntryName, Goal_3, Meta2, Meta3, Opts))
-        ;   call_on_stream0(Arch, EntryName, Goal_3, Meta2, Meta3, Opts)
+        ->  once(call_on_stream0(Arch, EntryName, Goal_3, Meta2, Meta3, SourceOpts))
+        ;   call_on_stream0(Arch, EntryName, Goal_3, Meta2, Meta3, SourceOpts)
         )
       ),
       archive_close(Arch)
     ),
     close_any(Close, Meta3, Meta4)
   ),
-  ignore(option(metadata(Meta4), Opts)).
+  ignore(option(metadata(Meta4), SourceOpts)).
 
 
-call_on_stream0(Arch, EntryName, Goal_3, Meta1, Meta4, Opts1) :-
-  merge_options([meta_data(MetaPath)], Opts1, Opts2),
-  archive_data_stream(Arch, In2, Opts2),
+call_on_stream0(Arch, EntryName, Goal_3, Meta1, Meta4, SourceOpts1) :-
+  merge_options([meta_data(Path)], SourceOpts1, SourceOpts2),
+  archive_data_stream(Arch, In2, SourceOpts2),
   % Even though the first input stream `In1` used UTF-8 encoding, the
   % streams that come out an any archive are `octet`.  This is
   % corrected here.
   set_stream(In2, encoding(utf8)),
-  (   MetaPath = [MetaEntry|_],
-      EntryName = MetaEntry.name
-  ->  Meta2 = Meta1.put(_{path: MetaPath}),
+  (   Path = [Entry|_],
+      EntryName = Entry.name
+  ->  put_dict(path, Meta1, Path, Meta2),
       call_cleanup(
         call(Goal_3, In2, Meta2, Meta3),
         close_any(close(In2), Meta3, Meta4)
@@ -132,7 +136,7 @@ call_on_stream0(Arch, EntryName, Goal_3, Meta1, Meta4, Opts1) :-
 
 
 %! call_onto_stream(+Source, +Sink, :Goal_4) is det.
-%! call_onto_stream(+Source, +Sink, :Goal_4, +Opts1, +Opts2) is det.
+%! call_onto_stream(+Source, +Sink, :Goal_4, +SourceOpts, +SinkOpts) is det.
 %
 % The following call is made: `call(Goal_4, In, Meta1, Meta2, Out)`.
 %
@@ -146,36 +150,41 @@ call_onto_stream(Source, Sink, Goal_4) :-
   call_onto_stream(Source, Sink, Goal_4, [], []).
 
 
-call_onto_stream(Source, Sink, Goal_4, InOpts, OutOpts) :-
-  call_on_stream(Source, call_onto_stream0(Sink, Goal_4, OutOpts), InOpts).
+call_onto_stream(Source, Sink, Goal_4, SourceOpts, SinkOpts) :-
+  call_on_stream(Source, call_onto_stream0(Sink, Goal_4, SinkOpts), SourceOpts).
 
 
-call_onto_stream0(Sink, Mod:Goal_4, OutOpts, In, Meta1, Meta2) :-
+call_onto_stream0(Sink, Mod:Goal_4, SinkOpts, In, Meta1, Meta2) :-
   Goal_4 =.. Comps1,
   append(Comps1, [In,Meta1,Meta2], Comps2),
   %%%%append(Comps0, [Meta1,Meta2], Comps2),
   Goal_1 =.. Comps2,
-  call_to_stream(Sink, Mod:Goal_1, OutOpts).
+  call_to_stream(Sink, Mod:Goal_1, SinkOpts).
 
 
 
 %! call_onto_streams(+Source, +Sink1, +Sink2, :Goal_5) is det.
-%! call_onto_streams(+Source, +Sink1, +Sink2, :Goal_5, +InOpts, +OutOpts1, +OutOpts2) is det.
+%! call_onto_streams(+Source, +Sink1, +Sink2, :Goal_5, +SourceOpts, +SinkOpts) is det.
+%! call_onto_streams(+Source, +Sink1, +Sink2, :Goal_5, +SourceOpts, +Sink1Opts, +Sink2Opts) is det.
 
 call_onto_streams(Source, Sink1, Sink2, Goal_5) :-
   call_onto_streams(Source, Sink1, Sink2, Goal_5, [], [], []).
 
 
-call_onto_streams(Source, Sink1, Sink2, Goal_5, InOpts, OutOpts1, OutOpts2) :-
-  call_on_stream(Source, call_onto_streams0(Sink1, Sink2, Goal_5, OutOpts1, OutOpts2), InOpts).
+call_onto_streams(Source, Sink1, Sink2, Goal_5, SourceOpts, SinkOpts) :-
+  call_onto_streams(Source, Sink1, Sink2, Goal_5, SourceOpts, SinkOpts, SinkOpts).
 
 
-call_onto_streams0(Sink1, Sink2, Mod:Goal_5, OutOpts1, OutOpts2, In, Meta1, Meta2) :-
+call_onto_streams(Source, Sink1, Sink2, Goal_5, SourceOpts, Sink1Opts, Sink2Opts) :-
+  call_on_stream(Source, call_onto_streams0(Sink1, Sink2, Goal_5, Sink1Opts, Sink2Opts), SourceOpts).
+
+
+call_onto_streams0(Sink1, Sink2, Mod:Goal_5, Sink1Opts, Sink2Opts, In, Meta1, Meta2) :-
   Goal_5 =.. Comps1,
   append(Comps1, [In,Meta1,Meta2], Comps2),
   %%%%append(Comps0, [Meta1,Meta2], Comps2),
   Goal_2 =.. Comps2,
-  call_to_streams(Sink1, Sink2, Mod:Goal_2, OutOpts1, OutOpts2).
+  call_to_streams(Sink1, Sink2, Mod:Goal_2, Sink1Opts, Sink2Opts).
 
 
 
@@ -190,7 +199,6 @@ call_to_atom(Goal_1, A) :-
 
 call_to_codes(Goal_1, Cs) :-
   call_to_something0(Goal_1, codes, Cs).
-
 
 
 call_to_something0(Goal_1, Type, Result) :-
@@ -218,7 +226,7 @@ memory_file_to_something0(Handle, string, Str) :- !,
 
 
 %! call_to_stream(+Sink, :Goal_1) is det.
-%! call_to_stream(+Sink, :Goal_1, +Opts) is det.
+%! call_to_stream(+Sink, :Goal_1, +SinkOpts) is det.
 %
 % The following options are supported:
 %
@@ -235,22 +243,23 @@ call_to_stream(Sink, Goal_1) :-
   call_to_stream(Sink, Goal_1, []).
 
 
-call_to_stream(Sink, Goal_1, Opts) :-
-  option(mode(Mode), Opts, append),
+call_to_stream(Sink, Goal_1, SinkOpts) :-
+  option(mode(Mode), SinkOpts, append),
   setup_call_cleanup(
     (
-      open_any(Sink, Mode, Out1, Close1, Opts),
-      write_stream_compression(Out1, Close1, Mode, Out2, Close2, Opts)
+      open_any(Sink, Mode, Out1, Close1, SinkOpts),
+      write_stream_compression(Out1, Close1, Mode, Out2, Close2, SinkOpts)
     ),
     call(Goal_1, Out2),
     close_any(Close2, Meta)
   ),
-  ignore(option(metadata(Meta), Opts)).
+  ignore(option(metadata(Meta), SinkOpts)).
 
 
 
 %! call_to_streams(+Sink1, +Sink2, :Goal_2) is det.
-%! call_to_streams(+Sink1, +Sink2, :Goal_2, +Opts1, +Opts2) is det.
+%! call_to_streams(+Sink1, +Sink2, :Goal_2, +SinkOpts) is det.
+%! call_to_streams(+Sink1, +Sink2, :Goal_2, +Sink1Opts, +Sink2Opts) is det.
 %
 % The following options are supported:
 %
@@ -268,15 +277,19 @@ call_to_streams(Sink1, Sink2, Goal_2) :-
   call_to_streams(Sink1, Sink2, Goal_2, [], []).
 
 
-call_to_streams(Sink1, Sink2, Goal_2, Opts1, Opts2) :-
-  option(mode(Mode1), Opts1, append),
-  option(mode(Mode2), Opts2, append),
+call_to_streams(Sink1, Sink2, Goal_2, SinkOpts) :-
+  call_to_streams(Sink1, Sink2, Goal_2, SinkOpts, SinkOpts).
+
+
+call_to_streams(Sink1, Sink2, Goal_2, Sink1Opts, Sink2Opts) :-
+  option(mode(Mode1), Sink1Opts, append),
+  option(mode(Mode2), Sink2Opts, append),
   setup_call_cleanup(
     (
-      open_any(Sink1, Mode1, Out1a, Close1a, Opts1),
-      write_stream_compression(Out1a, Close1a, Mode1, Out1b, Close1b, Opts1),
-      open_any(Sink2, Mode2, Out2a, Close2a, Opts2),
-      write_stream_compression(Out2a, Close2a, Mode2, Out2b, Close2b, Opts2)
+      open_any(Sink1, Mode1, Out1a, Close1a, Sink1Opts),
+      write_stream_compression(Out1a, Close1a, Mode1, Out1b, Close1b, Sink1Opts),
+      open_any(Sink2, Mode2, Out2a, Close2a, Sink2Opts),
+      write_stream_compression(Out2a, Close2a, Mode2, Out2b, Close2b, Sink2Opts)
     ),
     call(Goal_2, Out1b, Out2b),
     (
@@ -284,8 +297,8 @@ call_to_streams(Sink1, Sink2, Goal_2, Opts1, Opts2) :-
       close_any(Close2b, Meta2)
     )
   ),
-  ignore(option(metadata(Meta1), Opts1)),
-  ignore(option(metadata(Meta2), Opts2)).
+  ignore(option(metadata(Meta1), Sink1Opts)),
+  ignore(option(metadata(Meta2), Sink2Opts)).
 
 
 
