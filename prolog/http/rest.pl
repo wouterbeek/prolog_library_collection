73382e2a32c669c3864338da43aee47b8d1cd113
Wouter Beek <me@wouterbeek.com>
2016-08-01 22:39:11 +0200

    CLEAN: Vastly simplified REST implementation @rest

--- prolog/http/rest.pl
+++ prolog/http/rest.pl
@@ -1,40 +1,36 @@
 :- module(
   rest,
   [
-    rest_handler/3,   % +Req, +HandleId, :Goal_3
-    rest_handler/4,   % +Req, +HandleId, :Exists_1, :Singular_3
-    rest_handler/5,   % +Req, +HandleId, :Exists_1, :Singular_3, :Plural_2
-    rest_mediatype/3, % +Method, +MediaTypes, :Plural_2
-    rest_mediatype/4  % +Method, +MediaTypes, +Res, :Singular_3
+    rest_handler/2, % +Req, :Plural_2
+    rest_handler/5  % +Req, +HandleId, :Exists_1, :Singular_3, :Plural_2
   ]
 ).
 
 /** <module> REST
 
 @author Wouter Beek
-@version 2016/02, 2016/04-2016/06
+@version 2016/02, 2016/04-2016/06, 2016/08
 */
 
 :- use_module(library(http/html_write)). % HTML meta.
 :- use_module(library(http/http_ext)).
 :- use_module(library(http/http_json)).
 :- use_module(library(iri/iri_ext)).
-:- use_module(library(true)).
+:- use_module(library(lists)).
 
 :- html_meta
    rest_call_or_exception(2, +, +),
    rest_call_or_exception(3, +, +, +),
-   rest_handler(+, +, 3),
-   rest_handler(+, +, 1, 3),
+   rest_handler(+, 2),
    rest_handler(+, +, 1, 3, 2),
-   rest_mediatype(+, +, 2),
-   rest_mediatype(+, +, +, 3).
+   rest_handler_mediatype(2, +, +),
+   rest_handler_mediatype(3, +, +, +).
 
 
 
 
 
-%! rest_call_or_exception(:Plural_2, +Method, +MediaType) is det.
+%! rest_call_or_exception(:Plural_2,   +Method, +MediaType      ) is det.
 %! rest_call_or_exception(:Singular_3, +Method, +MediaType, +Res) is det.
 
 rest_call_or_exception(Plural_2, Method, MT) :-
@@ -63,24 +59,15 @@ rest_exception(_, E) :-
 
 
 
-%! rest_handler(+Req, +HandleId, :Goal_3) is det.
-%! rest_handler(+Req, +HandleId, :Exists_1, :Singular_3) is det.
-%! rest_handler(+Req, +HandleId, :Exists_1, :Singular_3, Plural_2) is det.
+%! rest_handler(+Req, :Plural_2) is det.
+%! rest_handler(+Req, +HandleId, :Exists_1, :Singular_3, :Plural_2) is det.
 
-rest_handler(Req, HandleId, Goal_3) :-
-  rest_handler(Req, HandleId, true, Goal_3).
-
-
-rest_handler(Req, _, Exists_1, Singular_3) :-
-  memberchk(request_uri(Local), Req),
+rest_handler(Req, Plural_2) :-
   http_accept(Req, MTs),
   http_method(Req, Method),
-  iri_to_resource(Local, Res),
-  call(Exists_1, Res),
-  call(Singular_3, Method, MTs, Res), !.
-rest_handler(Req, _, _, _) :-
-  memberchk(request_uri(Res), Req),
-  http_status_reply(Req, not_found(Res)).
+  call(Plural_2, Method, MTs), !.
+rest_handler(Req, _) :-
+  rest_not_found(Req).
 
 
 rest_handler(Req, HandleId, Exists_1, Singular_3, Plural_2) :-
@@ -89,50 +76,38 @@ rest_handler(Req, HandleId, Exists_1, Singular_3, Plural_2) :-
   http_method(Req, Method),
   http_link_to_id(HandleId, Endpoint),
   (   Local == Endpoint
-  ->  call(Plural_2, Method, MTs)
+  ->  rest_handler_mediatype(Plural_2, Method, MTs)
   ;   iri_to_resource(Local, Res),
       call(Exists_1, Res)
   ->  call(Singular_3, Method, MTs, Res)
   ), !.
 rest_handler(Req, _, _, _, _) :-
+  rest_not_found(Req).
+
+
+
+%! rest_handler_mediatype(:Plural_2,   +Method, +MTs      ) is det.
+%! rest_handler_mediatype(:Singular_3, +Method, +MTs, +Res) is det.
+
+rest_handler_mediatype(Plural_2, Method, MTs) :-
+  member(MT, MTs),
+  rest_call_or_exception(Plural_2, Method, MT), !.
+rest_handler_mediatype(_, _, _) :-
+  why_not_acceptable(Why),
+  http_status_reply(not_acceptable(Why)).
+
+
+rest_handler_mediatype(Singular_3, Method, MTs, Res) :-
+  member(MT, MTs),
+  rest_call_or_exception(Singular_3, Method, MT, Res), !.
+rest_handler_mediatype(_, _, _, _) :-
+  why_not_acceptable(Why),
+  http_status_reply(not_acceptable(Why)).
+
+
+rest_not_found(Req) :-
   memberchk(request_uri(Res), Req),
   http_status_reply(Req, not_found(Res)).
-
-
-% No media type is specified.  Anything goes.
-rest_mediatype(Method, [], Plural_2) :-
-  rest_call_or_exception(Plural_2, Method, _), !.
-% Some media type(s) is/are specified.  Try them in descending order of
-% preference.
-rest_mediatype(Method, MTs, Plural_2) :-
-  rest_mediatype0(Method, MTs, Plural_2).
-
-% None of the specified media types could be matched.  Give an error.
-rest_mediatype0(_, [], _) :- !,
-  why_not_acceptable(Why),
-  http_status_reply(not_acceptable(Why)).
-rest_mediatype0(Method, [MT|_], Plural_2) :-
-  rest_call_or_exception(Plural_2, Method, MT), !.
-rest_mediatype0(Method, [_|MTs], Plural_2) :-
-  rest_mediatype0(Method, MTs, Plural_2).
-
-
-% No media type is specified.  Anything goes.
-rest_mediatype(Method, [], Res, Singular_3) :-
-  rest_call_or_exception(Singular_3, Method, _, Res), !.
-% Some media type(s) is/are specified.  Try them in descending order of
-% preference.
-rest_mediatype(Method, MTs, Res, Singular_3) :-
-  rest_mediatype0(Method, MTs, Res, Singular_3).
-
-% None of the specified media types could be matched.  Give an error.
-rest_mediatype0(_, [], _, _) :- !,
-  why_not_acceptable(Why),
-  http_status_reply(not_acceptable(Why)).
-rest_mediatype0(Method, [MT|_], Res, Singular_3) :-
-  rest_call_or_exception(Singular_3, Method, MT, Res), !.
-rest_mediatype0(Method, [_|MTs], Res, Singular_3) :-
-  rest_mediatype0(Method, MTs, Res, Singular_3).
 
 
 why_not_acceptable(p("No acceptable media type could be served.")).
