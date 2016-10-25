650042a35dd491191d314ef21615f67f648cae00
Wouter Beek <me@wouterbeek.com>
2016-10-26 01:04:18 +0900

    ADDED: create_date_time_file/3 @date_time_file

--- prolog/os/date_time_file.pl
+++ prolog/os/date_time_file.pl
@@ -3,6 +3,7 @@
   [
     create_date_directory/2, % +Spec, -Dir
     create_date_time_file/2, % +Spec, -File
+    create_date_time_file/3, % +Spec, +Ext, -File
     is_older_file/2,         % +Path1, +Path2
     is_younger_file/2,       % +Path1, +Path2
     latest_date_time_file/2  % +Spec, -File
@@ -12,7 +13,7 @@
 /** <module> Date-time file support
 
 @author Wouter Beek
-@version 2015/07, 2015/10-2015/12, 2016/02-2016/03
+@version 2015/07, 2015/10-2015/12, 2016/02-2016/03, 2016/10
 */
 
 :- use_module(library(apply)).
@@ -25,13 +26,14 @@
 
 
 
-%! create_date_directory(+Spec:compound, -Directory) is det.
-% Create and return the current date subdirectory of the given absolute
-% directory name.
+%! create_date_directory(+Spec, -Dir) is det.
+%
+% Create and return the current date subdirectory of the given
+% absolute directory name.
 %
 % Example: `/tmp' ⇒ `/tmp/2013/05/10'
 
-create_date_directory(Spec, Dir):-
+create_date_directory(Spec, Dir) :-
   get_time(TS),
   format_time(atom(D), "%d", TS),
   format_time(atom(Mo), "%m", TS),
@@ -43,9 +45,10 @@ create_date_directory(Spec, Dir):-
 
 
 
-%! create_date_time_file(+Spec:compound, -File) is det.
+%! create_date_time_file(+Spec, -File) is det.
+%! create_date_time_file(+Spec, +Exts, -File) is det.
 
-create_date_time_file(Spec, File):-
+create_date_time_file(Spec, File) :-
   create_date_directory(Spec, Dir),
   get_time(TS),
   format_time(string(H), "%H", TS),
@@ -55,10 +58,15 @@ create_date_time_file(Spec, File):-
   directory_file_path(Dir, Local, File).
 
 
+create_date_time_file(Spec, Exts, File) :-
+  create_date_time_file(Spec, Base),
+  file_name_extensions(File, Base, Exts).
+
+
 
 %! is_older_file(+Path1, +Path2) is semidet.
 
-is_older_file(Path1, Path2):-
+is_older_file(Path1, Path2) :-
   is_younger_file(Path2, Path1).
 
 
@@ -67,7 +75,7 @@ is_older_file(Path1, Path2):-
 % Succeeds if the file denoted by Path1 is younger than
 % the file denoted by Path2.
 
-is_younger_file(Path1, Path2):-
+is_younger_file(Path1, Path2) :-
   time_file(Path1, T1),
   time_file(Path2, T2),
   T1 > T2.
@@ -76,7 +84,7 @@ is_younger_file(Path1, Path2):-
 
 %! latest_date_time_file(+Spec, -File) is det.
 
-latest_date_time_file(Spec, Latest):-
+latest_date_time_file(Spec, Latest) :-
   absolute_file_name(Spec, Dir, [access(read),file_type(directory)]),
 
   % Year
