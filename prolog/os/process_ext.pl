a79081f2f6d998f02c8bde8e4fd53c6aabcae247
Wouter Beek <wouter@triply.cc>
2016-12-31 14:35:08 +0100

    CLEAN: cleanup of module process_ext

--- prolog/os/process_ext.pl
+++ prolog/os/process_ext.pl
@@ -37,21 +37,6 @@ Additional support for starting processes from within SWI-Prolog:
 :- dynamic
     thread_id/3.
 
-:- predicate_options(run_jar/3, 3, [
-     pass_to(handle_process/3, 3)
-   ]).
-:- predicate_options(run_process/3, 3, [
-     detached(+boolean),
-     pass_to(run_process_inner/3, 3)
-   ]).
-:- predicate_options(run_process_inner/3, 3, [
-     error_goal(+callable),
-     output_goal(+callable),
-     program(+atom),
-     status(-nonneg),
-     pass_to(process_create/3, 3)
-   ]).
-
 :- meta_predicate
     run_jar(+, +, :),
     run_process(+, +, :).
@@ -78,6 +63,7 @@ renice(Pid, N):-
 
 %! run_jar(+Jar:atom, +Args) is det.
 %! run_jar(+Jar:atom, +Args, +Opts) is det.
+%
 % Runs the given JAR file with the given commandline arguments.
 %
 % Options are passed to run_process/3.
@@ -85,36 +71,37 @@ renice(Pid, N):-
 run_jar(Jar, Args):-
   run_jar(Jar, Args, []).
 
-run_jar(Jar, Args, Opts1):-
-  meta_options(is_meta, Opts1, Opts2),
 
-  % Set the program option.
-  file_base_name(Jar, JarName),
-  format(atom(Program), 'Java/JAR ~a', [JarName]),
-  merge_options([program(Program)], Opts2, Opts3),
-
-  run_process(java, ['-jar',file(Jar)|Args], Opts3).
+run_jar(Jar, Args, Opts0):-
+  meta_options(is_meta, Opts0, Opts),
+  run_process(java, ['-jar',file(Jar)|Args], Opts).
 
 
 
-%! run_process(+Process:atom, +Args:list) is det.
-%! run_process(+Process:atom, +Args:list, +Opts) is det.
-% Calls an external process with the given name and arguments,
-% and call an internal goal on the output that is coming from
-% the external process.
+%! run_process(+Process, +Args) is det.
+%! run_process(+Process, +Args, +Opts) is det.
+%
+% Calls an external process with the given name and arguments, and
+% call an internal goal on the output that is coming from the external
+% process.
 %
 % The following options are supported:
+%
 %   * detached(+boolean)
-%     Whether to run the process in a detached thread or not.
-%     Default is `false`.
-%   * error_goal(:Goal)
+%
+%     Whether to run the process in a detached thread or not.  Default
+%     is `false`.
+%
+%   * error_goal(+callable)
+%
 %     Call this goal on the error stream.
-%   * output_goal(:Goal)
+%
+%   * output_goal(+callable)
+%
 %     Call this goal on the output stream.
-%   * program(+Program:atom)
-%     The name of the program as displayed in debug messages.
-%     Default is Process.
-%   * status(-Status:nonneg)
+%
+%   * status(-nonneg)
+%
 %   * Other options are passed to process_create/3.
 %
 % @tbd Output and error is separate threads.
@@ -135,28 +122,26 @@ run_process(Process, Args, Opts1):-
   ).
 
 run_process_inner(Process, Args, Opts1):-
-  % By default the program name is the process name.
-  option(program(Program), Opts1, Process),
-  
   % Make sure the PID can be returned as an option, if given.
   (   select_option(process(Pid), Opts1, Opts2)
   ->  true
   ;   Opts2 = Opts1
   ),
   
-  % Filter out those options that can be processed by process_create/3.
+  % Filter out those options that can be processed by
+  % process_create/3.
   include(process_create_option, Opts2, Opts3),
   
-  % Make sure that the options for the PID, standard error
-  % and standard output are always handled to process_create/3.
+  % Make sure that the options for the PID, stderr and stdout are
+  % always handled to process_create/3.
   merge_options(
     Opts3,
     [process(Pid),stderr(pipe(ErrorStream)),stdout(pipe(OutputStream))],
     Opts4
   ),
   
-  % Allow process to be either an absolute file or a file
-  % relative to path.
+  % Allow process to be either an absolute file or a file relative to
+  % path.
   (   is_absolute_file_name(Process)
   ->  Exec = Process
   ;   Exec = path(Process)
@@ -186,7 +171,7 @@ run_process_inner(Process, Args, Opts1):-
       with_mutex(process_ext, (
         process_wait(Pid, exit(PidStatus))
       )),
-      process_status(Program, PidStatus),
+      process_status(PidStatus),
       (   option(status(PidStatus0), Opts2)
       ->  PidStatus0 = PidStatus
       ;   true
@@ -217,24 +202,25 @@ process_create_option(env(_)).
 process_create_option(priority(_)).
 process_create_option(window(_)).
 
-%! process_status(+Program, +Status:or([compound,nonneg])) is det.
-% Handling of exit codes given by programs that are run from the shell.
+%! process_status(+Status:or([compound,nonneg])) is det.
 %
-% @arg Program
-% @arg Status Either an integer status code,
-%      or an integer status code wrapped in functor `exit`.
+% Handling of exit codes given by programs that are run from the
+% shell.
 %
-% @throws shell_error Throws a shell error when a shell process exits with
-%         a non-zero code.
+% @arg Status Either an integer status code, or an integer status code
+%      wrapped in functor `exit`.
+%
+% @throws shell_error Throws a shell error when a shell process exits
+%         with a non-zero code.
 
 % Unwrap code.
-process_status(Program, exit(StatusCode)):- !,
-  process_status(Program, StatusCode).
+process_status(exit(StatusCode)):- !,
+  process_status(StatusCode).
 % Success code.
-process_status(_, 0):- !.
+process_status(0):- !.
 % Error/exception code.
-process_status(Program, StatusCode):-
-  print_message(warning, process_status(Program,StatusCode)).
+process_status(StatusCode):-
+  print_message(warning, process_status(StatusCode)).
 
 %! thread_status(
 %!   +Pid:nonneg,
@@ -253,6 +239,7 @@ thread_status(Pid, StreamType, exited(Term)):-
 
 
 %! kill_processes is det.
+%
 % Kills all running processes by PID.
 
 kill_processes:-
@@ -273,13 +260,14 @@ kill_processes:-
 
 % MESSAGES %
 
-:- multifile(prolog:message//1).
+:- multifile
+    prolog:message//1.
 
-prolog:message(process_status(Program,StatusCode)) -->
-  ['Program ~w returned status code ~w.'-[Program,StatusCode]].
+prolog:message(process_status(StatusCode)) -->
+  ["Process returned status code ~w."-[StatusCode]].
 prolog:message(thread_status(Pid,StreamType,fail)) -->
-  ['The ~a stream of process ~d failed.'-[StreamType,Pid]].
+  ["The ~a stream of process ~d failed."-[StreamType,Pid]].
 prolog:message(thread_status(Pid,StreamType,exception,Term)) -->
-  ['The ~a stream of process ~d threw exception ~w '-[StreamType,Pid,Term]].
+  ["The ~a stream of process ~d threw exception ~w "-[StreamType,Pid,Term]].
 prolog:message(thread_status(Pid,StreamType,exited,Term)) -->
-  ['The ~a stream of process ~d exited with ~w '-[StreamType,Pid,Term]].
+  ["The ~a stream of process ~d exited with ~w "-[StreamType,Pid,Term]].
