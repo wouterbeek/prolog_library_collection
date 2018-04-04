39ff18762b16207e8472c7258fbbcf7d87e2d1d5
Wouter Beek <wouter@triply.cc>
2018-04-04 09:22:17 +0200

    FIXED: XML DOM cleaning for empty atoms.

--- prolog/xml_ext.pl
+++ prolog/xml_ext.pl
@@ -1,8 +1,8 @@
 :- module(
   xml_ext,
   [
-    call_on_xml/3,      % +In, +RecordNames, :Goal_1
-    call_on_xml/4,      % +In, +RecordNames, :Goal_1, +Options
+    call_on_xml/3,      % +In, +ElementNames, :Goal_1
+    call_on_xml/4,      % +In, +ElementNames, :Goal_1, +Options
     load_xml/2,         % +Source, -Dom
     xml_encoding/2,     % +In, -Encoding
     xml_file_encoding/2 % +File, -Encoding
@@ -34,21 +34,21 @@
 
 
 
-%! call_on_xml(+In:stream, +RecordNames:list(atom), :Goal_1) is det.
-%! call_on_xml(+In:stream, +RecordNames:list(atom), :Goal_1,
+%! call_on_xml(+In:stream, +ElementNames:list(atom), :Goal_1) is det.
+%! call_on_xml(+In:stream, +ElementNames:list(atom), :Goal_1,
 %!             +Options:list(compound)) is det.
 %
 % Call Goal_1 on an XML stream, where the argument supplied to Goal_1
-% is a subtree that starts with an element called RecordName.
+% is a subtree that starts with an element called ElementName.
 %
 % @arg Options are set with set_sgml_parser/2.
 
-call_on_xml(In, RecordNames, Goal_1) :-
-  call_on_xml(In, RecordNames, Goal_1, []).
+call_on_xml(In, ElementNames, Goal_1) :-
+  call_on_xml(In, ElementNames, Goal_1, []).
 
 
-call_on_xml(In, RecordNames, Goal_1, Options1) :-
-  b_setval(xml_stream_record_names, RecordNames),
+call_on_xml(In, ElementNames, Goal_1, Options1) :-
+  b_setval(xml_stream_record_names, ElementNames),
   b_setval(xml_stream_goal, Goal_1),
   merge_options(Options1, [space(remove)], Options2),
   setup_call_cleanup(
@@ -60,28 +60,29 @@ call_on_xml(In, RecordNames, Goal_1, Options1) :-
     free_sgml_parser(Parser)
   ).
 
-on_begin0(Elem, Attr, Parser) :-
+on_begin0(ElementName, Attr, Parser) :-
   b_getval(xml_stream_goal, Goal_1),
-  b_getval(xml_stream_record_names, Elems),
-  memberchk(Elem, Elems), !,
+  b_getval(xml_stream_record_names, ElementNames),
+  memberchk(ElementName, ElementNames), !,
   sgml_parse(Parser, [document(Dom1),parse(content)]),
   xml_clean_dom(Dom1, Dom2),
-  (   call(Goal_1, [element(Elem,Attr,Dom2)])
+  (   call(Goal_1, [element(ElementName,Attr,Dom2)])
   ->  true
-  ;   print_message(warning, xml_error(element(Elem,Attr,Dom2)))
+  ;   print_message(warning, xml_error(element(ElementName,Attr,Dom2)))
   ).
 
-xml_clean_dom([element(Elem,Attr,Dom1)|T1], [element(Elem,Attr,Dom2)|T2]) :- !,
+xml_clean_dom([element(ElementName,Attr,Dom1)|T1], [element(ElementName,Attr,Dom2)|T2]) :- !,
   xml_clean_dom(Dom1, Dom2),
   xml_clean_dom(T1, T2).
 % Strip all blanks from the beginning and end of all strings.
-xml_clean_dom([H1|T1], [H2|T2]) :- !,
-  atom_strip(H1, H2),
-  xml_clean_dom(T1, T2).
-% Remove all strings that are empty (after stripping blanks form
-% beginning and end).
-xml_clean_dom([H|T1], T2) :-
-  is_empty_atom(H), !,
+xml_clean_dom([H1|T1], L2) :-
+  atom_strip(H1, H2), !,
+  (   % Remove all strings that are empty (after stripping blanks form
+      % beginning and end).
+      is_empty_atom(H2)
+  ->  L2 = T2
+  ;   L2 = [H2|T2]
+  ),
   xml_clean_dom(T1, T2).
 xml_clean_dom([], []).
 
