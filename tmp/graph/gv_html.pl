fbc9432b9aabf5c156fd912621a3c0daffb0650d
Wouter Beek <wouter@triply.cc>
2018-05-10 23:31:55 +0200

    CLEAN: Moved graph export to repository `graph_export'.

--- tmp/graph/gv_html.pl
+++ tmp/graph/gv_html.pl
@@ -26,36 +26,6 @@ gv_html_like_label(Content) --> "<", label(Content), ">".
 
 
 %! cell(+Contents:compound)// is det.
-%
-% Supported attributes for `TD`:
-%
-%   - `ALIGN="CENTER|LEFT|RIGHT|TEXT"`
-%   - `BALIGN="CENTER|LEFT|RIGHT"`
-%   - `BGCOLOR="color"`
-%   - `BORDER="value"`
-%   - `CELLPADDING="value"`
-%   - `CELLSPACING="value"`
-%   - `COLOR="color"`
-%   - `COLSPAN="value"`
-%   - `FIXEDSIZE="FALSE|TRUE"`
-%   - `GRADIENTANGLE="value"`
-%   - `HEIGHT="value"`
-%   - `HREF="value"`
-%   - `ID="value"`
-%   - `PORT="portName"`
-%   - `ROWSPAN="value"`
-%   - `SIDES="value"`
-%   - `STYLE="value"`
-%   - `TARGET="value"`
-%   - `TITLE="value"`
-%   - `TOOLTIP="value"`
-%   - `VALIGN="MIDDLE|BOTTOM|TOP"`
-%   - `WIDTH="value"`
-%
-% Supported attributes for `IMG`:
-%
-%   - `SCALE="FALSE|TRUE|WIDTH|HEIGHT|BOTH"`
-%   - `SRC="value"`
 
 cell(td(Contents)) --> !,
   cell(td([],Contents)).
@@ -76,8 +46,6 @@ cells([])       --> "".
 
 
 %! label(+Content:compound)// is det.
-%
-% GraphViz HTML-like label.
 
 label(Content) --> table(Content), !.
 label(Content) --> text(Content).
@@ -99,52 +67,6 @@ rows([]) --> "".
 
 
 %! table(+Contents:compound)// is det.
-%
-% ```
-% table : [ <FONT> ] <TABLE> rows </TABLE> [ </FONT> ]
-% ```
-%
-% Supported attributes for `TABLE`:
-%
-%   - `ALIGN="CENTER|LEFT|RIGHT"`
-%   - `BGCOLOR="color"`
-%   - `BORDER="value"`
-%   - `CELLBORDER="value"`
-%   - `CELLPADDING="value"`
-%   - `CELLSPACING="value"`
-%   - `COLOR="color"`
-%   - `COLUMNS="value"`
-%   - `FIXEDSIZE="FALSE|TRUE"`
-%   - `GRADIENTANGLE="value"`
-%   - `HEIGHT="value"`
-%   - `HREF="value"`
-%   - `ID="value"`
-%   - `PORT="portName"`
-%   - `ROWS="value"`
-%   - `SIDES="value"`
-%   - `STYLE="value"`
-%   - `TARGET="value"`
-%   - `TITLE="value"`
-%   - `TOOLTIP="value"`
-%   - `VALIGN="MIDDLE|BOTTOM|TOP"`
-%   - `WIDTH="value"`
-%
-% Supported attributes for `FONT`:
-%
-%  - COLOR="color"
-%
-%    Sets the color of the font within the scope of
-%    `<FONT>...</FONT>', or the border color of the table or cell
-%    within the scope of `<TABLE>...</TABLE>', or `<TD>...</TD>'.
-%    This color can be overridden by a `COLOR' attribute in
-%    descendents.  By default, the font color is determined by the
-%    `fontcolor' attribute of the corresponding node, edge or graph,
-%    and the border color is determined by the color attribute of the
-%    corresponding node, edge or graph.
-%
-%   - FACE="fontname"
-%
-%   - POINT-SIZE="value"
 
 table(table(Contents)) --> !,
   table(table([],Contents)).
@@ -163,11 +85,6 @@ table(font(Attrs1,Table)) -->
 
 
 %! text(+Contents:list)// .
-%
-% ```
-% text :   textitem
-%        | text textitem
-% ```
 
 text(Contents) -->
   {is_list(Contents)}, !,
@@ -178,29 +95,6 @@ text(Content) -->
 
 
 %! textitem(+Content:compound)// .
-%
-% ```
-% textitem :   string
-%            | <BR/>
-%            | <FONT> text </FONT>
-%            | <I> text </I>
-%            | <B> text </B>
-%            | <U> text </U>
-%            | <O> text </O>
-%            | <SUB> text </SUB>
-%            | <SUP> text </SUP>
-%            | <S> text </S>
-% ```
-%
-% Supported attributes for BR:
-%
-%   * `ALIGN="CENTER|LEFT|RIGHT"`
-%
-% Supported attributes for FONT:
-%
-%   * COLOR="color"
-%   * FACE="fontname"
-%   * POINT-SIZE="value"
 
 textitem(br(Attrs)) --> !,
   html_element(br, Attrs).
