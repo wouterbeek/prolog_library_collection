2bb4c090e61b956d1c92b0bb5f2330e200d3a3f5
Wouter Beek <wouter@triply.cc>
2020-04-21 21:29:42 +0200

    CHANGED: Had to rename the Github repo to fix SWI's naming scheme.

--- pack.pl
+++ pack.pl
@@ -2,7 +2,7 @@ author('Wouter Beek', 'wouter@triply.cc').
 download('https://github.com/wouterbeek/Prolog-Library-Collection/releases/*.zip').
 home('https://github.com/wouterbeek/Prolog-Library-Collection').
 maintainer('Wouter Beek', 'wouter@triply.cc').
-name(plc).
+name(prolog_library_collection).
 packager('Wouter Beek', 'wouter@triply.cc').
 title('Prolog Library Collection').
-version('1.0.1').
+version('1.0.2').
