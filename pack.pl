2d03753c650191b678ebb73627af5cccbd8b8d09
Wouter Beek <wouter@triply.cc>
2020-04-22 08:30:41 +0200

    FIXED: Keep package name consistent.

--- pack.pl
+++ pack.pl
@@ -1,8 +1,8 @@
 author('Wouter Beek', 'wouter@triply.cc').
-download('https://github.com/wouterbeek/Prolog-Library-Collection/releases/*.zip').
-home('https://github.com/wouterbeek/Prolog-Library-Collection').
+download('https://github.com/wouterbeek/prolog_library_collection/releases/*.zip').
+home('https://github.com/wouterbeek/prolog_library_collection').
 maintainer('Wouter Beek', 'wouter@triply.cc').
 name(prolog_library_collection).
 packager('Wouter Beek', 'wouter@triply.cc').
 title('Prolog Library Collection').
-version('1.0.3').
+version('1.0.4').
